fbe6dd1cf2caab27a72cb2f0a7b9ca4620c0738f
김지연 <sarakjykjy@naver.com>
2017-07-25 15:43:32 -0500

    Update ServerUtil.java

--- Awake10805/app/src/main/java/com/example/awake/ServerUtil.java
+++ Awake10805/app/src/main/java/com/example/awake/ServerUtil.java
@@ -4,6 +4,6 @@ package com.example.awake;
  * Created by 지연 on 2016-05-02.
  */
 public class ServerUtil {
-    public static final String SERVER_URL="http://14.63.215.152/MindWave/";
-   // public static final String SERVER_URL="http://192.168.0.7/MindWave/";
+    public static final String SERVER_URL="http://localhost/MindWave/";
+   // public static final String SERVER_URL="http://localhost/MindWave/";
 }
